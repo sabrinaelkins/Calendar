2241533794d38274bf6c82f914c0599cd0c811e6
sabrinaelkins <sabrina.elkins12@gmail.com>
2021-10-21 14:48:28 -0500

    save schedule

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 $(document).ready(function () {
 
-var currentDate = moment().format("MMM Do YY");  
-$("#currentDay").text(currentDate)
-$(".saveplan").on("click",function(){
-var timeBlock = $(this).attr("id").split("-")[0]
-var userSchedule = $("#" + timeBlock + "-plan").val()
-console.log(timeBlock, userSchedule)
-localStorage.setItem(timeBlock, userSchedule)
-})
+    var currentDate = moment().format("MMM Do YY");
+    $("#currentDay").text(currentDate)
+    $(".saveplan").on("click", function () {
+        var timeBlock = $(this).attr("id").split("-")[0]
+        var userSchedule = $("#" + timeBlock + "-plan").val()
+        console.log(timeBlock, userSchedule)
+        localStorage.setItem(timeBlock, userSchedule)
+    })
+    var currentHour = moment().hour()
+    for (let i = 9; i < 19; i++) {
+        var userSchedule = localStorage.getItem(i)
+        $("#" + i + "-plan").val(userSchedule)
+    }
 })
