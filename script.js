45e3cc1678d70d7ab655d84df1fa8bc9b23578ed
sabrinaelkins <sabrina.elkins12@gmail.com>
2021-10-21 14:38:49 -0500

    time blocks working

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ $(document).ready(function () {
 
 var currentDate = moment().format("MMM Do YY");  
 $("#currentDay").text(currentDate)
+$(".saveplan").on("click",function(){
+var timeBlock = $(this).attr("id").split("-")[0]
+var userSchedule = $("#" + timeBlock + "-plan").val()
+console.log(timeBlock, userSchedule)
+
+})
 })
